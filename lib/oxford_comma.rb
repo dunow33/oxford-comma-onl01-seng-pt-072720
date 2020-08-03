111a5c45436bfba4577c03d67a187ffda5249191
Brian Donnoe <dunow33@hotmail.com>
2020-08-03 04:16:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,26 +7,6 @@ def oxford_comma(arrayArg)
     last = (arrayArg.length - 1)
     temp = arrayArg[last]
     arrayArg[last] = "and " + temp
-    #arrayArg.push(temp)
-    
     return arrayArg.join(", ")
-    #string1 = arrayArg.join(", ")
-    #string1 << " and "
-    #return string1
-    #newArray = string1.split(", ")
-  
-    #newArray.each_with_index do |elem, index|
-     # if index == (newArray.length-1)
-      #  newArray.push("and")
-      #else
-      #  newArray.push(elem)
-      #end
-  
+  end
 end
-  
-end
-
-
-array1 = ["56", "89", "67", "78", "43", "893", "444"]
-
-puts oxford_comma(array1)
