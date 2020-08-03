2488664e87cedfda4693d5c49580af99160dd195
Brian Donnoe <dunow33@hotmail.com>
2020-08-03 03:18:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,12 @@
 def oxford_comma(array)
   to_string = array.join(",")
   
-  counter = 0
-  array.each do |elem|
-    if counter == (array.length-1)
-  end
+  return to_string
+  
+  #counter = 0
+  #array.each do |elem|
+   # if counter == (array.length-1)
+  #end
 end
 
 array1 = ["3", "5", "7", "98"]
