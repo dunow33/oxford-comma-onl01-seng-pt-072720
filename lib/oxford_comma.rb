7a7e8b04c61813d76420757ea3d92033cd6b9090
Brian Donnoe <dunow33@hotmail.com>
2020-08-03 03:46:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,16 +4,10 @@ def oxford_comma(arrayArg)
   elsif(arrayArg.size == 2)
    return arrayArg.join(" and ")
   elsif
+    string1 = arrayArg.join(",")
+    newArray = string1.split(",")
   
-  #string1 = arrayArg.join(",")
-  #newArray = string1.split(",")
-  
-  #counter = 0
-  #array.each_with_index do |elem|
-   #if counter == (array.length-1)
-  end
+    array.each_with_index do |elem, index|
+      if counter == (array.length-1)
+      end
 end
-
-#array1 = ["3"]
-
-#puts oxford_comma(array1)
