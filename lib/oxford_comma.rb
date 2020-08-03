b5098402c4369a6ff60065aa1223a6caab236fba
Brian Donnoe <dunow33@hotmail.com>
2020-08-03 03:47:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(arrayArg)
     string1 = arrayArg.join(",")
     newArray = string1.split(",")
   
-    newArrayarray.each_with_index do |elem, index|
+    newArray.each_with_index do |elem, index|
       if index == (array.length-1)
       end
 end
